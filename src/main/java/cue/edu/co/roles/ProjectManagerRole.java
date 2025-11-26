c0e6fc8dd7c495171b0a9b02f9499021c3d42f2a
Valentina <valenhoyitos2019@gmail.com>
2025-11-25 20:03:42 -0500

    feat(SegregacionInterfaces): crear interfaces segregadas para roles específicos

---
+++ src/main/java/cue/edu/co/roles/ProjectManagerRole.java
@@ -0,0 +1,9 @@
+package cue.edu.co.roles;
+
+/**
+ * ISP: Interfaz pequeña, solo para gestión de proyectos.
+ */
+public interface ProjectManagerRole {
+
+    void assignProject(String projectName);
+}
